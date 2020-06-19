82e81ab035cf428b1c5918296210032b98f0490f
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-06-19 16:05:47 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,9 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
-  if position_taken?()
-    
-
+  if board[index] >=0 && board[index] <= 8 && position_taken?() == false
+    true
+  end
 end
 
 
